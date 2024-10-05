f592571bd94553337c9b0674c50e23ca46d26787
Duyduide <duydante311@gmail.com>
2024-10-05 22:53:49 +0700

    #2 Handle error

--- server/models/user.js
+++ server/models/user.js
@@ -1,38 +1,50 @@
 const mongoose = require('mongoose'); // Erase if already required
-
+const bcrypt = require('bcrypt');
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
-    },
-    lastName:{
-        type:String,
-        required:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    cart: {
-        type: Array,
-        default: [],
-    },
-    address: {
-        type: Array,
-        default: [],
+let userSchema = new mongoose.Schema({
+        firstname:{
+            type:String,
+            required:true,
+        },
+        lastname:{
+            type:String,
+            required:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+        mobile:{
+            type:String,
+            required:true,
+            unique:true,
+        },
+        password:{
+            type:String,
+            required:true,
+        },
+        role: {
+            type: String,
+            default: 'user',
+        },
+        cart: {
+            type: Array,
+            default: [],
+        },
+        address: {
+            type: Array,
+            default: [],
+        }
+    }, 
+        {timestamps: true}
+);
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) {
+        next();
     }
-});
-
+    const salt = bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 //Export the model
 module.exports = mongoose.model('User', userSchema);	
--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 require('dotenv').config();
 const dbConnect = require('./config/dbconnect');
+const initRoutes = require('./routes'); 
 
 const app = express();
 const port = process.env.PORT || 8888
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// Ket noi database
 dbConnect();
-
-app.use('/', (req, res) => {
-    res.send('hello world');
-})
+initRoutes(app);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
