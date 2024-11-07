bb06f09b3a06a739324f6575b5336a42e0090d05
bptn1919 <125757961+bptn1919@users.noreply.github.com>
2024-11-07 16:26:54 +0700

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import { RiPhoneFill } from "react-icons/ri";
+import { CgMail } from "react-icons/cg";
 const Header = () => {
   return (
-    <div className='border w-main h-[110px] py-[35px]'>Header</div>
-  )
-}
+    <div className="bg-blue-100 w-full">
+      <div className=" w-main h-[110px] py-[35px]  mx-auto flex items-center justify-start">
+ 
+        {/* logo */}
+        <img src="logoabc.png" alt="Cart" className="h-[80px] w-auto" />
+         <div className = "flex text-[13px] ml-auto space-x-8">
+          <div className = 'flex flex-col items-center'>
+          <span className = 'flex gap-3 items-center'>
+          <RiPhoneFill color = 'red' />
+          <span className = 'font-semibold'>(+1800) 000 6606</span>
+          </span>
+          <span> Mon-Sat 9:00AM - 8:00PM </span>
+          </div>
+          <div className = 'flex flex-col items-center'>
+          <span className = 'flex gap-3 items-center'>
+          <CgMail color = 'red' />
+          <span className = 'font-semibold'>ABCSUPPORT@GMAIL.COM</span>
+          </span>
+          <span> Online Support 24/7 </span>
+          </div>
+          </div> 
+      </div>
+    </div>
+  );
+};
 
 export default Header
