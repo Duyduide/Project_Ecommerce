f70221c2814b6da392b485596294731f52b7cfbf
longhang2004 <long.hangnhut2004@hcmut.edu.vn>
2024-11-10 21:29:19 +0700

    Change cart
    Lúc trước tui làm cart riêng vì suy nghĩ theo hướng SQL =)). Giờ thấy ông Duy gắn cart array trong user nên chuyển qua cái đó :v

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -7,4 +7,10 @@ router.get('/queryCart/:userID', verifyAccessToken , cartController.queryCartOfU
 router.delete('/deleteProductFromCart', verifyAccessToken, cartController.deleteProductFromCart);
 router.put('/updateProductQuantity', verifyAccessToken, cartController.changeCartProductQuantity);
 
+//dùng mấy cái dưới này nha, mấy cái bên trên là tui làm kiểu cũ :v
+router.post('/addToUserCart', verifyAccessToken , cartController.addToUserCart);
+router.get('/queryUserCart/:userID', verifyAccessToken , cartController.queryUserCart);
+router.delete('/deleteProductFromUserCart', verifyAccessToken, cartController.deleteProductFromUserCart);
+router.put('/updateUserCartProductQuantity', verifyAccessToken, cartController.changeUserCartProductQuantity);
+
 module.exports = router;
