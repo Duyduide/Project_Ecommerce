67ec84fd4f4af9a0968ee80ce37b3a2ec1210f94
longhang2004 <long.hangnhut2004@hcmut.edu.vn>
2024-11-11 21:39:18 +0700

    change order

--- server/routes/order.js
+++ server/routes/order.js
@@ -6,7 +6,8 @@ router.post('/createOrder', verifyAccessToken, orderController.createOrder);
 router.get('/queryOrderOfUser', verifyAccessToken, orderController.queryOrderOfUser);
 router.get('/queryOrderById/:orderID', verifyAccessToken, orderController.queryOrderById);
 // router.put('/updateOrder', orderController.updateOrderById); //admin only
-router.delete('/deleteOrder', verifyAccessToken, orderController.deleteOrderById);
+router.put('/cancelOrder', verifyAccessToken, orderController.cancelOrderById);
 //thêm vài cái modify order cho admin
+router.delete('/deleteOrder', verifyAccessToken, orderController.deleteOrderById);
 
 module.exports = router;
