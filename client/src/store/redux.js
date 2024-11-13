4dc915518dace1efebc73acfaee266c82ec19cf6
longhang2004 <long.hangnhut2004@hcmut.edu.vn>
2024-11-13 16:46:45 +0700

    add redux (chưa test)

--- client/src/store/redux.js
+++ client/src/store/redux.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appSlice from './app/appSlice';
 import storage from 'redux-persist/lib/storage';
+import productListSlice from './product/productListSlice'
 
 import { persistReducer, persistStore } from 'redux-persist';
 import userSlice from './user/userSlice'
@@ -18,7 +19,8 @@ const userConfig = {
 export const store = configureStore({
   reducer: {
     app: appSlice,
-    user: persistReducer(userConfig, userSlice)
+    user: persistReducer(userConfig, userSlice),
+    product: productListSlice
   },
 });
 
