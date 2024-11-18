62d372bf7634ca7f33162e80f3aa2e11246149e1
longhang2004 <long.hangnhut2004@hcmut.edu.vn>
2024-11-18 09:28:30 +0700

    update cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const cartController = require('../controllers/cartController');
 const { verifyAccessToken } = require('../middlewares/verifyToken');
 
-router.post('/addToCart', verifyAccessToken , cartController.addToCart);
-router.get('/queryCart/:userID', verifyAccessToken , cartController.queryCartOfUser);
-router.delete('/deleteProductFromCart', verifyAccessToken, cartController.deleteProductFromCart);
-router.put('/updateProductQuantity', verifyAccessToken, cartController.changeCartProductQuantity);
+// router.post('/addToCart', verifyAccessToken , cartController.addToCart);
+// router.get('/queryCart/:userID', verifyAccessToken , cartController.queryCartOfUser);
+// router.delete('/deleteProductFromCart', verifyAccessToken, cartController.deleteProductFromCart);
+// router.put('/updateProductQuantity', verifyAccessToken, cartController.changeCartProductQuantity);
 
 //dùng mấy cái dưới này nha, mấy cái bên trên là tui làm kiểu cũ :v
 router.post('/addToUserCart', verifyAccessToken , cartController.addToUserCart);
