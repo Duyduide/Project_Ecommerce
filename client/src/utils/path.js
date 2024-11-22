7cce58005c5a3898e2c0812aba85b53c0f2ff783
Duyduide <duydante311@gmail.com>
2024-11-22 11:35:35 +0700

    add admin pages

--- client/src/utils/path.js
+++ client/src/utils/path.js
@@ -9,6 +9,18 @@ const path = {
     RESET_PASSWORD: '/reset-password/:token',
     DETAIL_CART: '/detail-cart',
     CHECKOUT: '/checkout',
+
+    // ADMIN
+    ADMIN: 'admin',
+    DASHBOARD: 'dasboard',
+    MANAGE_USERS: 'manage-users',
+    MANAGE_PRODUCTS: 'manage-products',
+    MANAGE_ORDERS: 'manage-orders',
+    CREATE_PRODUCT: 'create-product',
+
+    // MEMBER
+    MEMBER: 'member',
+    PERSONAL: 'personal',
 }
 
 export default path;
